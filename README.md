0c167de536e268fff250d4687b652567ebf57201
JabbaSciMan <49959525+JabbaSciMan@users.noreply.github.com>
2023-04-17 16:44:42 -0600

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Open this page at [https://jabbasciman.github.io/gonzalez-4-button-contest/](https://jabbasciman.github.io/gonzalez-4-button-contest/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/jabbasciman/gonzalez-4-button-contest** and import
+
+## Edit this project ![Build status badge](https://github.com/jabbasciman/gonzalez-4-button-contest/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/jabbasciman/gonzalez-4-button-contest** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/jabbasciman/gonzalez-4-button-contest/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,56 @@
+// red button pin 5
+input.onButtonPressed(Button.A, function () {
+    if (!(won)) {
+        won = true
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        music.playMelody("E D G F B A C5 B ", 120)
+        strip.clear()
+        strip.show()
+        won = false
+    }
+})
+// Yellow Button
+// 
+input.onPinPressed(TouchPin.P2, function () {
+    if (!(won)) {
+        won = true
+        strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+        music.playMelody("C5 A B G A F G E ", 120)
+        strip.clear()
+        strip.show()
+        won = false
+    }
+})
+// Blue Button pin
+input.onButtonPressed(Button.B, function () {
+    if (!(won)) {
+        won = true
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+        music.playMelody("C5 G B A F A C5 B ", 120)
+        strip.clear()
+        strip.show()
+        won = false
+    }
+})
+// Green Button
+input.onPinPressed(TouchPin.P1, function () {
+    if (!(won)) {
+        won = true
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        music.playMelody("C5 B A G F E D C ", 120)
+        strip.clear()
+        strip.show()
+        won = false
+    }
+})
+let strip: neopixel.Strip = null
+let won = false
+won = false
+strip = neopixel.create(DigitalPin.P8, 27, NeoPixelMode.RGB)
+strip.showRainbow(1, 360)
+basic.pause(2000)
+strip.clear()
+strip.show()
+basic.forever(function () {
+	
+})
